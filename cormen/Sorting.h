a69cd752ff447fdb05d8994576139aac2539b3e5
vahagnbabajanyan <vahagndrive6@gmail.com>
2016-05-06 20:14:38 +0400

    Second commit

--- cormen/Sorting.h
+++ cormen/Sorting.h
@@ -1,6 +1,7 @@
 #ifndef CORMEN_SORTING_H_20160507_1124
 #define CORMEN_SORTING_H_20160507_1124
 
+#include "stdafx.h"
 #include <iostream>
 
 namespace cormen
@@ -8,11 +9,30 @@ namespace cormen
 
 	namespace sorting
 	{
+
+		/*
+		*  ----------- Merge Sort : Begin ------------------
+		*/
 		template <typename Container>
-		void merge_sort(int begin, int end, Container& c)
+		void merge(int beg, int mid, int end, Container& c)
 		{
-			std::cout << "Hello From Cormen Lib" << std::endl;
+
 		}
+
+		template <typename Container>
+		void merge_sort(int beg, int end, Container& c)
+		{
+			if (beg < end)
+			{
+				int mid = (beg + end) / 2;
+				merge_sort(beg, mid, c);
+				merge_sort(mid + 1, end, c);
+				merge(beg, mid, end, c);
+			}
+		}
+		/*
+		* ----------- Merge Sort : End ----------------------
+		*/
 
 	} // namespace sorting
 
