b565c1af3950eadcb36b9a2a46667c394937e3eb
vahagnbabajanyan <vahagndrive6@gmail.com>
2016-05-10 23:07:38 +0400

    First commit from home machine

--- evacuation/main.cpp
+++ evacuation/main.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-	std::cout << "Evacuation Main" << std::endl;
+	std::cout << "First change on home machine" << std::endl;
 
 	std::vector<int> v{10, 4, 7, 2, 8, 3, 12, 42, 5, 6, 82, 12, 22};
 	cormen::sorting::merge_sort(0, static_cast<std::uint32_t>(v.size() - 1), v);
