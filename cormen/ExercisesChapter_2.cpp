90e5c610fb82b1962963d5863f73495313655d59
vahagnbabajanyan <vahagndrive6@gmail.com>
2016-05-12 19:11:06 +0400

    Exercise 2.3.2

---
+++ cormen/ExercisesChapter_2.cpp
@@ -0,0 +1,43 @@
+#include "stdafx.h"
+
+#include "ExercisesChapter_2.h"
+
+namespace
+{
+	namespace ex3_2
+	{
+		template <typename Container>
+		void merge(std::int32_t b, std::int32_t m, std::int32_t e, Container& c)
+		{
+
+		}
+
+		template <typename Container>
+		void merge_sort(std::int32_t beg, std::int32_t end, Container& c)
+		{
+
+		}
+
+	} // namespace ex3_2
+
+} // anonymous namespace
+
+namespace cormen
+{
+
+	namespace execrises
+	{
+
+		namespace chapter_2
+		{
+			
+			void Paragraf3::Ex3_2()
+			{
+
+			}
+
+		} //namespace chapter_2
+
+	} // namespace exercises
+
+} // namespace cormen
---
+++ cormen/ExercisesChapter_2.h
@@ -0,0 +1,26 @@
+#ifndef CORMENLIB_EXERCISES_CHAPTER_2_H
+#define CORMENLIB_EXERCISES_CHAPTER_2_H
+
+namespace cormen
+{
+
+	namespace execrises
+	{
+
+		namespace chapter_2
+		{
+
+			class Paragraf3
+			{
+			public:
+				static void Ex3_2();
+
+			}; // class Paragraf3
+
+		} // namespace chapter_2
+
+	} // namespace execrises
+
+} // namespace cormen
+
+#endif
